030c2062c47e4ebc1357a0f74cc8ca5117c03169
jit-12345 <jitchak95@gmail.com>
2024-01-20 21:05:23 +0530

    Frst view of js ->video 54

---
+++ VIDEO54_JS/new.js
@@ -0,0 +1,5 @@
+var a=5;
+var b=7;
+ console.log(a+b);
+
+ 
---
+++ VIDEO54_JS/script.js
@@ -0,0 +1,17 @@
+alert("Hello World !!!")
+
+console.log("Code Is Running ...")
+console.log("Code Is Running Like Wow")
+
+
+var a=prompt("Enter the value of a")
+console.log("The value of a is "+a)
+
+var isTrue=confirm("If ok then it will bern your computer")
+
+if(isTrue){
+    console.log("Your computer is burning")
+}
+else{
+    console.log("Your computer is not burning")
+}
