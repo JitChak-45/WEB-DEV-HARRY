3c39cfa8c7359957571d85bfc986a0ef7846aadc
jit-12345 <jitchak95@gmail.com>
2024-01-21 17:49:22 +0530

    loops in js

---
+++ VIDEO57_JS/index.js
@@ -0,0 +1,44 @@
+console.log("Here we see some example of loops in JS");
+
+
+//1......for loop
+/*for(let i=1;i<=10;i++){
+    console.log(i);
+}*/
+
+//2......for in loop
+/*let obj={
+    name:"Jit",
+    role:"Developer",
+    Company:"Google"
+}
+
+for (const key in obj) {
+        // const element = obj[key];
+        // console.log(key,element);
+
+        const val=obj[key];
+        console.log(key,val);
+} */
+
+//3......forof loop
+/*for (const i of "Jit Chakraborty") {
+    console.log(i);
+}*/
+
+//4......while loop
+
+/*let a=1;
+while(a<6){
+    console.log(a);
+    a++;
+}*/
+
+
+//5......do while loop
+/*
+let i=0;
+do{
+    console.log(i+1);
+    i++;
+}while(i<5)*/
