264565fa6c0abba15b04749b8b77fba234f3b601
jit-12345 <jitchak95@gmail.com>
2024-01-21 18:32:41 +0530

    function,arrow function

---
+++ VIDEO58_JS/index.js
@@ -0,0 +1,49 @@
+console.log("Need of function");
+
+//in the below example you need multiple line to use this same complemnts in diifernt person
+
+/*console.log("Hey Jit you are a good guy !!");
+console.log("Hey Jit you are a good developer !!");
+console.log("Hey Jit your t-shirt looks awesome !!");
+console.log("Hey Jit you have a leadership skill !!");*/
+
+//creating a primary function
+
+
+/*function changeName(name) {
+    console.log("Hey "+name+ " you are a good guy !!");
+console.log("Hey " +name+ " you are a good developer !!");
+console.log("Hey " +name+ " your t-shirt looks awesome !!");
+console.log("Hey " +name+ " you have a leadership skill !!");
+}
+
+changeName("Jit");
+changeName("Ipsita");
+changeName("Vendi");*/
+
+
+//function to add two numbers
+
+/*function sum(a,b,c=3){
+    console.log(a,b,c);
+    return a+b+c;
+}
+
+result1=sum(4);
+result2=sum(2,3);
+result3=sum(1,3,1);
+
+console.log("Sum of items are : ",result1);//NaN means not a number here b is undefinde at )(30 line)
+console.log("Sum of items are : ",result2);
+console.log("Sum of items are : ",result3);*/
+
+//arrow function
+
+const func1=(x)=>{
+    console.log("I am a arrow function : ",x)
+}
+
+func1(3);
+func1(45);
+func1(54);
+
