ce308c47ad96c13e56577e03ab7f7b74a8d08be9
jit-12345 <jitchak95@gmail.com>
2024-01-26 23:51:27 +0530

    some other remaining

---
+++ VIDEO63_JS/loops.js
@@ -0,0 +1,35 @@
+let a=[93,4,1,10,2];
+
+/*for loop*/
+// for(let i=0;i<a.length;i++){
+//     console.log(a[i]);
+//     // const ele=a[i];
+//     // console.log(ele);
+// }
+
+
+/*forEach loop*/
+// a.forEach((value,index,array)=>{
+//     console.log(value,index,array);
+// })
+
+/*forin loop*/
+
+// let obj={
+//     a:1,
+//     b:2,
+//     c:3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key,element);
+        
+//     }
+// }
+
+/*forof loop*/
+
+// for (const val of a) {
+//     console.log(val);
+// }
---
+++ VIDEO63_JS/script.js
@@ -0,0 +1,79 @@
+let arr=["alu","potol","dab",7,false];
+//print the whole array
+console.log(arr);//[ 'alu', 'potol', 'dab', 7, false 
+//to print the array length
+console.log(arr.length);//5
+
+//to show the array index wise(accessing the values)
+console.log(arr[2]);//dab
+
+//change the values
+arr[2]="kochi dab";
+console.log(arr);//[ 'alu', 'potol', 'kochi dab', 7, false ]
+
+//in javascript array are objects
+console.log(typeof arr);//object
+
+//by the help of toString() method array is changed into string
+console.log(arr.toString());//alu,potol,kochi dab,7,false
+
+//join("") methods help to join element of array
+console.log(arr.join(""));//alupotolkochi dab7false
+console.log(arr.join("-"));//alu-potol-kochi dab-7-false
+
+//pop() method pop the element from last
+console.log(arr.pop());//false
+console.log(arr);//after pop -->[ 'alu', 'potol', 'kochi dab', 7 ]
+
+//push() method push the value at the last
+arr.push(true)
+console.log(arr);//[ 'alu', 'potol', 'kochi dab', 7, true ]
+
+//shift() remove element from first
+arr.shift();
+console.log(arr);//[ 'potol', 'kochi dab', 7, true ]
+
+//unshift() method add value at the front
+arr.unshift("alu");
+console.log(arr);//[ 'alu', 'potol', 'kochi dab', 7, true ]
+
+
+// pop() is brother of shift()
+// push() is brother of unshift()
+
+let a=[1,2];
+let b=["ma","baba"];
+
+//concat() function concatinate arrays --> does not change the original array(exception)
+console.log(arr.concat(a,b));//[ 'alu', 'potol', 'kochi dab', 7, true, 1, 2, 'ma', 'baba' ]
+
+//delete() function delete the element ar]t the given index
+let a1=[4,2,8,1,3];
+delete a1[a1.length-1];
+console.log(a1);//[ 4, 2, 8, 1, <1 empty item> ]-->because it aquire space upto that
+
+//asign value
+a1[a1.length-1]=5;
+console.log(a1);//[ 4, 2, 8, 1, 5 ]
+
+//sort()
+console.log(a1.sort());//[ 1, 2, 4, 5, 8 ]-->it changes the actual array
+
+//splice()
+// [4,2,8,1,3]
+
+a1.splice(1,2);
+console.log(a1);//[ 1, 5, 8 ]
+
+a1.splice(1,0,23,45);
+console.log(a1);//[ 1, 23, 45, 5, 8 ]
+
+//slice()
+console.log(a1.slice(2));
+console.log(a1.slice(2,4));//2 included 4 excluded
+
+//reverse()-->reverse the array
+console.log(a1.reverse());// [ 8, 5, 45, 23, 1 ]
+
+
+
