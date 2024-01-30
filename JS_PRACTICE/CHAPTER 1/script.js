bc644589d47efe3773cf8827d817e3af93845462
jit-12345 <jitchak95@gmail.com>
2024-01-31 00:01:30 +0530

    HARRY'S NOTE CHAPTER1 EXERCISE OF JS

---
+++ JS_PRACTICE/CHAPTER 1/script.js
@@ -0,0 +1,27 @@
+let s="jit";
+console.log(s.concat(1));
+s=s+5;
+console.log(s);
+console.log(typeof s);
+
+const a=7;
+//asigning value to constant value is not allowed
+// a=8;
+console.log(a);
+
+const obj={
+    "name":"Jit",
+    "age of Jit":22
+}
+console.log(obj);
+obj.salary="20000";
+console.log(obj);
+
+const obj2={
+    "apple":"a fruit",
+    "Jit":"A human",
+    "Ram":"God",
+    "Cow":"Ma Parbati",
+    "Sita":"Wife of Ram"
+}
+console.log(obj2);
