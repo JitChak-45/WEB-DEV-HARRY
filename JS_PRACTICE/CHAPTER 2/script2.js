deaf2b64d43cc6afc2c895f04c8b2826acd04da8
jit-12345 <jitchak95@gmail.com>
2024-01-31 00:44:05 +0530

    EXERCISE 2->QUESTION 2

---
+++ JS_PRACTICE/CHAPTER 2/script2.js
@@ -0,0 +1,12 @@
+let ch=prompt("Enter any character");
+
+switch (ch) {
+   
+    case ch:
+          alert(`The charcter is ${ch}`);     
+        break;
+
+    default:
+        alert("The value is null");
+        break;
+}
