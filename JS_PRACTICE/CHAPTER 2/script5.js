f9b0c6e6c7d80f5a0ea041c7fca279d4bc1d1038
jit-12345 <jitchak95@gmail.com>
2024-01-31 00:58:46 +0530

    Exercise 2->Question 5

---
+++ JS_PRACTICE/CHAPTER 2/script5.js
@@ -0,0 +1,9 @@
+let age=prompt("Enter your age");
+
+age>=18?alert("you can drive"):alert("you can't drive");
+
+
+//another method
+let age2=confirm("is your age 18 or greater?");
+
+age2==true?alert("you can drive"):alert("you can't drive");
