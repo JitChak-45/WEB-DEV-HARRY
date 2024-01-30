daafbdef371ae7a2bda0d1a281514c11f2f57155
jit-12345 <jitchak95@gmail.com>
2024-01-31 00:51:00 +0530

    Exercise 2->question 4

---
+++ JS_PRACTICE/CHAPTER 2/script4.js
@@ -0,0 +1,8 @@
+let num=prompt("Enter any number to check divisible by 2 and 3");
+
+if(num%2==0 || num%3==0){
+    alert("The number is divisble by 2 or 3");
+}
+else{
+    alert("Not divisible by 2 or 3");
+}
