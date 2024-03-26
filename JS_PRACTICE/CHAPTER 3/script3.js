b1281eb9cab0f60532bc83aa625463f12a581532
Jit Chakraborty <jitchak95@gmail.com>
2024-03-26 10:45:54 -0700

    3rd exercise

---
+++ JS_PRACTICE/CHAPTER 3/script3.js
@@ -0,0 +1,12 @@
+
+while(true){
+    let num=prompt("Enter Rohit Sharma jersy number!!!");
+    if(num!=45){
+        alert("Try Again!");
+    }
+    else{
+        alert("You Got It!!");
+        break;
+    }
+    
+}
