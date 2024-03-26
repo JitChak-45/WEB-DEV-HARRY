48322586a05ecffc9eedb2d0c4f1704a4e6999bf
Jit Chakraborty <jitchak95@gmail.com>
2024-03-25 21:10:37 -0700

    loop practice

---
+++ JS_PRACTICE/CHAPTER 2/Practice 2nd time/loop.js
@@ -0,0 +1,15 @@
+// for (const i of "jitchak45") {
+//     console.log(i);
+// }
+
+let obj={
+    "Employee Name":"Jit",
+    "Employee id":"45",
+    "Employee Department":"CyberSecurity",
+    "Employee Designation":"SDE-III"
+}
+for (const key in obj) {
+    // const value=obj[key];
+    console.log(key);
+    // console.log(key,value);
+}
