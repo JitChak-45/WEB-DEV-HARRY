81e3758975f0199a21da6511e373fdf694f44190
Jit Chakraborty <jitchak95@gmail.com>
2024-03-27 11:13:01 -0700

    children,siblings,parents in dom

---
+++ VIDEO67_JS/script.js
@@ -0,0 +1,33 @@
+console.log(document.body);
+console.log(document.body.firstChild);
+console.log(document.body.lastChildChild);
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1].childNodes);
+
+const cont=document.body.childNodes[1]
+console.log(cont);
+
+console.log(cont.childNodes);
+// div is a element but text is not an Element
+console.log(cont.firstElementChild);
+
+cont.lastElementChild.style.backgroundColor="red";
+cont.lastElementChild.parentElement.style.backgroundColor="blue"
+
+// it shows only elements
+document.body.firstElementChild.children
+// get 0 element 
+document.body.firstElementChild.children[0]
+
+//previous element sibling and next element sibling
+
+document.body.firstElementChild.children[3].nextElementSibling;
+document.body.firstElementChild.children[3].previousElementSibling;
+
+//not elements also the nodes
+document.body.firstElementChild.children[3].previousSibling;
+
+document.body.firstElementChild.children[3].parentElement;
+
+// children of table 
+document.body.firstElementChild.children[3].parentElement.parentElement.children[1];
