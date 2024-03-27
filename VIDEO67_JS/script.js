adfde611ab9e3e7d3d196b6576b1ef299676a389
Jit Chakraborty <jitchak95@gmail.com>
2024-03-27 12:42:55 -0700

    some errors

--- VIDEO67_JS/script.js
+++ VIDEO67_JS/script.js
@@ -1,6 +1,6 @@
 console.log(document.body);
 console.log(document.body.firstChild);
-console.log(document.body.lastChildChild);
+console.log(document.body.lastChild);
 console.log(document.body.childNodes);
 console.log(document.body.childNodes[1].childNodes);
 
