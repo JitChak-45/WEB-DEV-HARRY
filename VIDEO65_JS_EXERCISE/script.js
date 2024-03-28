a8144d232fbc0ab5cc18e3e7180d3b3d3f44157d
jit-12345 <jitchak95@gmail.com>
2024-03-28 23:36:07 +0530

    commiting harrys solution

--- VIDEO65_JS_EXERCISE/script.js
+++ VIDEO65_JS_EXERCISE/script.js
@@ -4,3 +4,23 @@ for(let i=6;i>=1;i--){
     ans*=i;
 }
 console.log(ans);
+
+// harrys solution -->
+let a = 7
+
+function factorial(number){
+   let arr = Array.from(Array(number+1).keys())
+   let c = arr.slice(1,).reduce((a, b)=> a*b )
+   return c
+}
+
+
+function facFor(number){
+    let fac = 1;
+    for (let index = 1; index <= number; index++) {
+        fac = fac * index
+    }
+    return fac
+}
+console.log(factorial(a))
+console.log(facFor(a))
