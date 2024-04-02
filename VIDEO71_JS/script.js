c19ed0c68572c1b6926adcf1dbbdbb7dc8d78330
Jit Chakraborty <jitchak95@gmail.com>
2024-04-02 07:28:15 -0700

    inserting and removing element by js

---
+++ VIDEO71_JS/script.js
@@ -0,0 +1,20 @@
+console.log(document.querySelector(".container").innerHTML);
+document.querySelector(".box");
+document.querySelector(".box").innerHTML;
+document.querySelector(".box").innerText;
+
+document.querySelector(".container").outerHTML;
+document.querySelector(".container").tagName;//it show only the elements
+document.querySelector(".container").nodeName;//it show the nodes not only the element
+document.querySelector(".container").textContent;//text - all the tags
+
+// document.querySelector(".container").hidden=true//hides the element
+
+// when we write document.querySelector(".box") then if there multiple Element present with 
+// same class name then the first box are automatically chosen
+
+document.querySelector(".box").innerHTML="Jit"
+//it changes the context of the box
+
+
+// /************ document.designMode="on" ***********/ very important command to edit any webpage
