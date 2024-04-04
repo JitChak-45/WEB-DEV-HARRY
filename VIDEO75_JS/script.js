da7abdafeb2386d1e026cf5f429fb9268e18afc9
Jit Chakraborty <jitchak95@gmail.com>
2024-04-04 10:48:20 -0700

    callback,asynchronus nature,promises

---
+++ VIDEO75_JS/script.js
@@ -0,0 +1,9 @@
+console.log("<------JavaScript Asynchronus Nature-------->");
+console.log("Harry is a Hacker!")
+console.log("Jit is a Hacker!")
+
+setTimeout(()=>{
+console.log("Inside of timeout function")
+},1000)
+
+console.log("The end of program");
