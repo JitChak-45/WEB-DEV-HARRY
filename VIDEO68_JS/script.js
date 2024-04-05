7a86789ade5db1a0b965e08dadf8dabb34d18a7a
jit-12345 <jitchak95@gmail.com>
2024-04-05 22:46:38 +0530

    some update on video 68

--- VIDEO68_JS/script.js
+++ VIDEO68_JS/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // let boxes=document.getElementsByClassName("box");
 
 // boxes[2].style.backgroundColor="crimson";
@@ -16,8 +15,3 @@ console.log(e[4].matches("#redbox"));//true
 console.log(e[2].closest(".container"));
 console.log(document.querySelector(".container").contains(e[2]))
 
-=======
-let boxes=document.getElementsByClassName("box");
-console.log(boxes);
-boxes[2].style.backgroundColor="red";
->>>>>>> 5bf80c3c639e0e795764b9965c45f46462337030
