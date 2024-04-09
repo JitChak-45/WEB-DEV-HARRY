9bce9934f3ef322f73a6ca82e60a9ebb9d77f275
jit-12345 <jitchak95@gmail.com>
2024-04-09 10:23:09 +0530

    video 78 exercise

--- VIDEO76_JS/script.js
+++ VIDEO76_JS/script.js
@@ -10,9 +10,23 @@
 
 async function getData() {
     //simulate promise to get data from a server
-    let x=fetch('https://jsonplaceholder.typicode.com/todos/1')
-   console.log(x);
-   return 455;
+    // let x= await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    // let data=await x.json(); //parsing of data
+    let x= await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+       
+    let data=await x.json();
+   console.log(data);
+   return x;
 }
 
 async function main(){
@@ -25,6 +39,7 @@ console.log("Do something else");
 console.log("Load data");
 
 let data = await getData()
+// let data = getData()
 
 console.log(data);
 
@@ -44,3 +59,28 @@ main()
 
 // console.log("task 2")
 // })
+
+/***settle** means promise is resolve or reject
+ if the promise is resolved then the promise is settled sucessfull
+ and if reject then the promise does not resolve sucessfull
+*/
+
+// Example POST method implementation:
+// async function postData(url = "", data = {}) {
+//     // Default options are marked with *
+//     const response = await fetch(url, {
+//       method: "POST",
+      
+//       headers: {
+//         "Content-Type": "application/json",
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+//       body: JSON.stringify(data), 
+//     });
+//     return response.json(); 
+//   }
+  
+//   postData("https://example.com/answer", { answer: 42 }).then((data) => {
+//     console.log(data); // JSON data parsed by `data.json()` call
+//   });
+  
