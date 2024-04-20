3cee272c034d07432d25154269e711ce8bb81c18
jit-12345 <jitchak95@gmail.com>
2024-04-20 20:25:26 +0530

    alert display

---
+++ JS_PRACTICE/Chapter 8/script.js
@@ -0,0 +1,13 @@
+let btn1=document.querySelector(".submit1");
+let btn2=document.querySelector(".submit2");
+let btn3=document.querySelector(".submit3");
+
+btn1.addEventListener(click,()=>{
+    alert("Box 1 content is submitted");
+})
+btn2.addEventListener(click,()=>{
+    alert("Box 2 content is submitted");
+})
+btn3.addEventListener(click,()=>{
+    alert("Box 3 content is submitted");
+})
