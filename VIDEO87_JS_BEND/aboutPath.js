b32c891d14088a599413c13a3624ae2fe6280f87
Jit Chakraborty <jitchak95@gmail.com>
2024-05-05 00:21:34 +0530

    path module updated

---
+++ VIDEO87_JS_BEND/aboutPath.js
@@ -0,0 +1,8 @@
+import path from "path"
+let mypath="//Users//jitchakraborty//Desktop//WEB-DEV-HARRY//VIDEO87_JS_BEND//Jit2.txt"
+// console.log(path);
+console.log(path.extname(mypath));
+
+console.log(path.dirname(mypath));
+console.log(path.basename(mypath));
+console.log(path.join("//Users","//VIDEO87_JS_BEND//Jit2.txt"))
