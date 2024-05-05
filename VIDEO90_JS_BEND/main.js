19876d6d21df082a0a51bbac2c2a2628e1b37dbc
jit-12345 <jitchak95@gmail.com>
2024-05-05 23:02:31 +0530

    middleware

---
+++ VIDEO90_JS_BEND/main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+    res.send('Hello About!')
+  })
+  app.get('/contact', (req, res) => {
+    res.send('Hello Contact!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
