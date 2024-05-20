2a319cd140eebb5c27560f2510c7e0665f086110
jit-12345 <jitchak95@gmail.com>
2024-05-20 09:58:21 +0530

    ejs

--- VIDEO92_JS_BEND/index.js
+++ VIDEO92_JS_BEND/index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    // console.log(__dirname);
-  res.sendFile("templetes/index.html",{root:__dirname});
+  let siteName="Adidas"
+  let searchText="Search Now"
+    // console.log(__dirname); 
+    let arr=[1,54,55]
+  res.render("index",{siteName:siteName,searchText:searchText,arr});
+})
+app.get('/blog/:slug', (req, res) => {
+  let blogTitle="Adidas why and when ?";
+  let blogContent="It is a very good brand"
+    // console.log(__dirname); 
+  res.render("blogpost",{blogTitle:blogTitle,blogContent:blogContent});
 })
 
 app.listen(port, () => {
