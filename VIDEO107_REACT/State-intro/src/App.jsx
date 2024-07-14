2b03f716766d009b0364a2f13bcf06eebfe8ea55
Jit Chakraborty <jitchak95@gmail.com>
2024-07-14 22:12:21 +0530

    change on state and hooks

--- VIDEO107_REACT/State-intro/src/App.jsx
+++ VIDEO107_REACT/State-intro/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const[count,setCount]=useState(2);//usestate is a hook
+  // let a=5; we can't do this
 
   //count:it is a state
   //setCount:it is a function which help to change the value of state
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <button onClick={()=>{setCount(count ** 2)}}>Update Value:{count}</button>
+      {/* <button onClick={()=>{a=a+1}}>Update Value:{a}</button> it is not possible */}
     </>
   )
 }
