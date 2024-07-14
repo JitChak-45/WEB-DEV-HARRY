40e15551a2113f3f59365a4ace715ada082275ed
Jit Chakraborty <jitchak95@gmail.com>
2024-07-14 22:08:11 +0530

    state and hook

--- VIDEO107_REACT/State-intro/src/App.jsx
+++ VIDEO107_REACT/State-intro/src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
   const[count,setCount]=useState(2);//usestate is a hook
 
+  //count:it is a state
+  //setCount:it is a function which help to change the value of state
+  //useState:It is a function which help to intitialize the value of the state
+
   return (
     <>
       <button onClick={()=>{setCount(count ** 2)}}>Update Value:{count}</button>
